913cc8f7b6c5b4ab1571127c949b4895c139f6e1
gde5090ti-design <gde5090ti@gmail.com>
2026-09-18 08:00:42 -0300

    Create App.js

---
+++ Src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Meu Projeto React</h1>
+      <p>Aplicação configurada e pronta para o deploy na Vercel!</p>
+    </div>
+  );
+}
+
+export default App;
